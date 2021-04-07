942b9509aba0fa27e9508ce23725d259e3a7166d
Leonnardo M <metalherogamer@gmail.com>
2021-04-07 20:58:15 -0300

    :package: Update

--- src/dashboard/server.js
+++ src/dashboard/server.js
@@ -1,49 +1,70 @@
-require('./strategies/discord')
-const express = require('express')
-const app = express()
-const path = require("path")
+require("./strategies/discord");
+const express = require("express");
+const app = express();
+const path = require("path");
 
-const client = require('../../index')
-const session = require("express-session")
-const mongoose = require("mongoose")
-const StoreMongo = require("connect-mongo")(session)
+const client = require("../../index");
+const session = require("express-session");
+const mongoose = require("mongoose");
+const StoreMongo = require("connect-mongo")(session);
 
-const passport = require("passport")
-const index = require('./routes/index')
+const passport = require("passport");
+const index = require("./routes/index");
 
 const { Webhook } = require("@top-gg/sdk");
 
-const webhook = new Webhook("LmAl27052004@@");
+const webhook = new Webhook(process.env.DBL_PASSWORD);
 
 app.post("/dblwebhook", webhook.middleware(), async (req, res) => {
-  console.log("voto detectado")
+  const { MessageEmbed } = require("discord.js");
+
+  const EMBED = new MessageEmbed()
+    .setTitle(`<a:DSL_Topgg:829504601980010516> Voto no Top.gg`)
+    .setDescription(
+      `**${
+        client.users.cache.get(req.vote.user).tag
+      }** votou no **Zafriel** no site **[Top.GG](https://top.gg/bot/601847636546289664)**`
+    )
+    .setFooter(req.vote.user)
+    .setTimestamp()
+    .setThumbnail(
+      client.user.cache
+        .get(req.vote.user)
+        .displayAvatarURL({ dynamic: true, format: "jpg", size: 2048 })
+    );
+
+  client.channels.cache
+    .get("795936120042684427")
+    .send(`<@${req.vote.user}>`, EMBED);
 });
 
-app.use(session({ //Configuração do express-session
-     secret: 'keyboard cat',
-     cookie: {
-        maxAge: 60000 * 60 * 24//tempo do cookie
+app.use(
+  session({
+    //Configuração do express-session
+    secret: "keyboard cat",
+    cookie: {
+      maxAge: 60000 * 60 * 24, //tempo do cookie
     },
     resave: false,
     saveUninitialized: false,
-    store: new StoreMongo({ mongooseConnection: mongoose.connection }) //pra o store mongo
-}))
+    store: new StoreMongo({ mongooseConnection: mongoose.connection }), //pra o store mongo
+  })
+);
 
-app.use(passport.initialize())//inicializa o  passport
-app.use(passport.session())//inicializa o session do passport
+app.use(passport.initialize()); //inicializa o  passport
+app.use(passport.session()); //inicializa o session do passport
 
+app.set("views", path.join(__dirname, "/views")); //diretorio de views
+app.use(express.static(path.join(__dirname, "/public"))); //diretorio de arquivos estaticos
 
-app.set("views", path.join(__dirname, "/views"))//diretorio de views
-app.use(express.static(path.join(__dirname, "/public")))//diretorio de arquivos estaticos
+app.engine("html", require("ejs").renderFile); //coloca ejs renderFile como engine
+app.set("view engine", "ejs"); //seta o ejs como view engine
 
-app.engine("html", require("ejs").renderFile) //coloca ejs renderFile como engine
-app.set("view engine", "ejs")//seta o ejs como view engine
+app.use(express.json()); //Pra receber respostas json
+app.use(express.urlencoded({ extended: true }));
 
-app.use(express.json())//Pra receber respostas json
-app.use(express.urlencoded({ extended: true }))
-
-app.use('/', index) //pasta de rotas
+app.use("/", index); //pasta de rotas
 
 app.listen(process.env.PORT, () => {
-  console.log('a dashboard tbm')
-})//liga servidor
+  console.log("a dashboard tbm");
+}); //liga servidor
--- src/eventos/ready.js
+++ src/eventos/ready.js
@@ -1,25 +1,28 @@
-const mongoose = require('mongoose')
-const { Collection, MessageEmbed } = require("discord.js")
+const mongoose = require("mongoose");
+const { Collection, MessageEmbed } = require("discord.js");
 
+module.exports.run = (client) => {
+  client.commands = new Collection(); //cria loc
+  client.aliases = new Collection();
 
-module.exports.run=  (client) => {
-    client.commands = new Collection();//cria loc
-    client.aliases = new Collection();
+  mongoose
+    .connect(`${process.env.MONGOURI}`, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+    })
+    .then(() => {
+      console.log("Conectado ao MongoDB");
+    })
+    .catch((err) => {
+      console.log(`Deu erro: ${err}`);
+    });
 
-  mongoose.connect(`${process.env.MONGOURI}`, { //loga no mongodb
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false
-  }).then(() => {
-    console.log("Conectado ao MongoDB")
-  }).catch((err) => {
-    console.log(`Deu erro: ${err}`)
-  })
-  
-  
-    require('../handler.js')(client);//executa a handler
+  require("../handler.js")(client); //executa a handler
 
-    console.log(`Relaxa Matheus, o pai ta on`);
-   
-  
-}
+  client.user.setStatus("dnd");
+
+  client.user.setActivity(`🕵️ | Verificando os Votos do Zafriel`, {
+    type: "PLAYING",
+  });
+};
